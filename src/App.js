fc9b85d60255a8c108091a52114c1086145de58d
לאון זולוטריוב <kinglion1964@gmail.com>
2023-12-27 21:17:53 +0200

    work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-
-import Home from './pages/Home/Home'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import Login from './pages/LoginRegister/Login';
 import BookTable from './pages/BookTable/BookTable';
 import Register from './pages/LoginRegister/Register';
-import { BrowserRouter , Routes ,Route} from 'react-router-dom';
+import Provider from './Context';
+
 function App() {
-  return(
-    <div className='App'>
-      
+  return (
+    <Provider>
       <BrowserRouter>
         <Routes>
-          <Route index element = {<Home />}/>
-          <Route path='/home' element= {<Home />}/>
-          <Route path='/login' element={<Login />} />
-          <Route path='/book_table' element={<BookTable />} />
-          <Route path='/register' element={<Register />} />
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/book_table" element={<BookTable />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </Provider>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './index.css';
 import App from './App';
-import { createRoot } from 'react-dom/client';
 import Provider from './Context';
+import {createRoot} from 'react-dom/client';
+import './index.css';
 
-const root = createRoot(document.getElementById('root'));
+
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-    <React.StrictMode>
-        <Provider>
-            <App/>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
