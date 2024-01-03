33e726cde3f1b86d311250eea80aa1c65cc0796a
לאון זולוטריוב <kinglion1964@gmail.com>
2024-01-03 21:25:40 +0200

    Update

--- src/pages/LoginRegister/Login.jsx
+++ src/pages/LoginRegister/Login.jsx
@@ -1,80 +1,93 @@
 import './LoginRegister.css';
 import React, { useContext, useState } from 'react';
 import { Context } from "../../Context";
-import { Route, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import BookTable from '../BookTable/BookTable';
 
 export default function Login() {
   const { state, dispatch } = useContext(Context);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    fetch("https://localhost:7213/api/Authentication", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    })
-      .then((result) => {
-        if (!result.ok) {
-          throw new Error(`Request failed with status: ${result.status}`);
-        }
-        return result.text();
-      })
-      .then((data) => {
-        dispatch({ type: "setToken", payload: data });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
   const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const result = await fetch("https://localhost:7213/api/Authentication", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+  
+      if (!result.ok) {
+        throw new Error(`Request failed with status: ${result.status}`);
+      }
+      
+      const data = await result.text();
+      console.log("Response from server:", data); 
+      dispatch({ type: "setToken", payload: data, userType: data.type });
+  
+      if (data) {
+        if (data.type === "Admin") {
+          navigate("/home");
+        } else {
+          navigate("/book_table");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
   const navigateRegister = () => {
     navigate("/register");
   };
+
   const navigateSite = () => {
     navigate("/");
   };
 
   return (
     <div className="loginRegister">
-      <form onSubmit={handleLogin}>
-        <h2>Login</h2>
-        <div className="auth_form_container">
-          <label htmlFor="email">email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="youreamil@******.com"
-            id="email"
-            name="email"
-          />
-          <label htmlFor="password">password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="********"
-            id="password"
-            name="password"
-          />
-          <button type="submit">Log In</button>
-           
-          <button className="link-btn" onClick={navigateRegister}>
-            Don't have an account? Register here.
-          </button>
-          <button className="button_SendHome" onClick={navigateSite}>
-            Back to Site
-          </button>
-        </div>
-      </form>
+      {!state.token ? (
+        <form onSubmit={handleLogin}>
+          <h2>Login</h2>
+          <div className="auth_form_container">
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="youreamil@******.com"
+              id="email"
+              name="email"
+            />
+            <label htmlFor="password">password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="********"
+              id="password"
+              name="password"
+            />
+            <button type="submit">Log In</button>
+                  <button className="link-btn" onClick={navigateRegister}>
+        Don't have an account? Register here.
+      </button>
+      <button className="button_SendHome" onClick={navigateSite}>
+        Back to Site
+      </button>
+          </div>
+        </form>
+      ) : (
+        <BookTable />
+      )}
     </div>
   );
 };
--- src/pages/LoginRegister/Register.jsx
+++ src/pages/LoginRegister/Register.jsx
@@ -1,49 +1,118 @@
 import './LoginRegister.css';
-import React , { useState }from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+export default function Register() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setphone] = useState('');
+  const [password, setpass] = useState('');
+  const [BD, setBD] = useState('');
 
-
-
-export default function Register () {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setphone] = useState('');
-    const [pass, setpass] = useState ('');
-    const [BD, setBD] = useState('');
-   const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    };
-    const navigate = useNavigate();
-    const navigateLogin = () => {
-        navigate('/login')
-    };
-    const navigateSite = () => {
-        navigate('/')
-    };
-    return(
-        <div className="loginRegister">
-            <form onSubmit={handleSubmit}>
-                <h2>Register</h2>
-                <div className="auth_form_container">
-                <label htmlFor="First Name">First Name</label>
-                <input value={firstName} onChange={(e) => setFirstName(e.target.value)}placeholder="First Name" id="First Name" name= "First Name" />
-                <label htmlFor="Last Name">Last Name</label>
-                <input value={lastName} onChange={(e) => setLastName(e.target.value)}placeholder="Last Name" id="Last Name" name= "Last Name" />
-                <label htmlFor="Email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="YourEmail@******.com" id="email" name= "email" />
-                <label htmlFor="Phone">Phone</label>
-                <input value={phone} onChange={(e) => setphone(e.target.value)}placeholder="Phone" id="Phone" name= "Phone" pattern="[0-9]{3}-[0-9]{4}-[0-9]{3}"></input>
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setpass(e.target.value)} type="password" placeholder="********" id="password" name= "password" />
-                <label htmlFor="BD">Birthday</label>
-                <input value={BD} onChange={(e) => setBD(e.target.value)}type="date" placeholder="BD" id="BD" name= "BD" />
-                <button type ="submit" >Sign In</button>
-                <button className="link-btn" onClick={navigateLogin}>Already have an account? Login here.</button>
-                <button className="button_SendHome" onClick={navigateSite}>Back to Site</button>
-                </div>
-            </form>
+    try {
+      const response = await fetch("https://localhost:7213/Register", {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          firstName: firstName,
+          lastName : lastName,
+          email: email,
+          phone : phone,
+          password : password,
+          BD : BD,
+        }),
+      });
+
+      const responseData = await response.json();
+
+      if (response.ok) {
+        console.log('הרישום הצליח!');
+      } else {
+        console.error('שגיאה במהלך הרישום:', responseData.error);
+        alert(responseData.error);
+      }
+    } catch (error) {
+      console.error('שגיאה במהלך הרישום:', error);
+    }
+  };
+
+  const navigate = useNavigate();
+  const navigateLogin = () => {
+    navigate('/login');
+  };
+  const navigateSite = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="loginRegister">
+      <form onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <div className="auth_form_container">
+          <label htmlFor="First Name">First Name</label>
+          <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="First Name"
+            id="First Name"
+            name="First Name"
+          />
+          <label htmlFor="Last Name">Last Name</label>
+          <input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder="Last Name"
+            id="Last Name"
+            name="Last Name"
+          />
+          <label htmlFor="Email">Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="YourEmail@******.com"
+            id="email"
+            name="email"
+          />
+          <label htmlFor="Phone">Phone</label>
+          <input
+            value={phone}
+            onChange={(e) => setphone(e.target.value)}
+            placeholder="Phone"
+            id="Phone"
+            name="Phone"
+            pattern="[0-9]{3}[0-9]{4}[0-9]{3}"
+          />
+          <label htmlFor="password">password</label>
+          <input
+            value={password}
+            onChange={(e) => setpass(e.target.value)}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+          />
+          <label htmlFor="BD">Birthday</label>
+          <input
+            value={BD}
+            onChange={(e) => setBD(e.target.value)}
+            type="date"
+            placeholder="BD"
+            id="BD"
+            name="BD"
+          />
+          <button type="submit">Sign In</button>
+          <button className="link-btn" onClick={navigateLogin}>
+            Already have an account? Login here.
+          </button>
+          <button className="button_SendHome" onClick={navigateSite}>
+            Back to Site
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
