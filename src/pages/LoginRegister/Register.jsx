b45f0146599147f91e9c89e1ac3d25a85fd50004
לאון זולוטריוב <kinglion1964@gmail.com>
2024-01-08 04:27:55 +0200

    update

--- src/pages/LoginRegister/Register.jsx
+++ src/pages/LoginRegister/Register.jsx
@@ -1,14 +1,15 @@
-import './LoginRegister.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './LoginRegister.css';
 
 export default function Register() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [phone, setphone] = useState('');
-  const [password, setpass] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
   const [BD, setBD] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,11 +19,11 @@ export default function Register() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           firstName: firstName,
-          lastName : lastName,
+          lastName: lastName,
           email: email,
-          phone : phone,
-          password : password,
-          BD : BD,
+          phone: phone,
+          password: password,
+          BD: BD,
         }),
       });
 
@@ -30,6 +31,7 @@ export default function Register() {
 
       if (response.ok) {
         console.log('הרישום הצליח!');
+        setSubmitted(true);
       } else {
         console.error('שגיאה במהלך הרישום:', responseData.error);
         alert(responseData.error);
@@ -43,76 +45,89 @@ export default function Register() {
   const navigateLogin = () => {
     navigate('/login');
   };
+
   const navigateSite = () => {
     navigate('/');
   };
 
   return (
+    <form className="loginRegister" onSubmit={handleSubmit}>
+        {submitted ? (
+          <>
+            <h1>Welcome, we will be happy to see you soon with us</h1>
+            <button className="button_SendHome" onClick={navigateSite}>
+                Back to Site
+              </button>
+          </>
+        ) : (
+          <>
     <div className="loginRegister">
-      <form onSubmit={handleSubmit}>
-        <h2>Register</h2>
-        <div className="auth_form_container">
-          <label htmlFor="First Name">First Name</label>
-          <input
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            placeholder="First Name"
-            id="First Name"
-            name="First Name"
-          />
-          <label htmlFor="Last Name">Last Name</label>
-          <input
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            placeholder="Last Name"
-            id="Last Name"
-            name="Last Name"
-          />
-          <label htmlFor="Email">Email</label>
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            placeholder="YourEmail@******.com"
-            id="email"
-            name="email"
-          />
-          <label htmlFor="Phone">Phone</label>
-          <input
-            value={phone}
-            onChange={(e) => setphone(e.target.value)}
-            placeholder="Phone"
-            id="Phone"
-            name="Phone"
-            pattern="[0-9]{3}[0-9]{4}[0-9]{3}"
-          />
-          <label htmlFor="password">password</label>
-          <input
-            value={password}
-            onChange={(e) => setpass(e.target.value)}
-            type="password"
-            placeholder="********"
-            id="password"
-            name="password"
-          />
-          <label htmlFor="BD">Birthday</label>
-          <input
-            value={BD}
-            onChange={(e) => setBD(e.target.value)}
-            type="date"
-            placeholder="BD"
-            id="BD"
-            name="BD"
-          />
-          <button type="submit">Sign In</button>
-          <button className="link-btn" onClick={navigateLogin}>
-            Already have an account? Login here.
-          </button>
-          <button className="button_SendHome" onClick={navigateSite}>
-            Back to Site
-          </button>
-        </div>
-      </form>
+            <div className="auth_form_container">
+            <h1>Register</h1>
+              <label htmlFor="firstName">First Name</label>
+              <input
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First Name"
+                id="firstName"
+                name="firstName"
+              />
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last Name"
+                id="lastName"
+                name="lastName"
+              />
+              <label htmlFor="email">Email</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="YourEmail@******.com"
+                id="email"
+                name="email"
+              />
+              <label htmlFor="phone">Phone</label>
+              <input
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                placeholder="Phone"
+                id="phone"
+                name="phone"
+                pattern="[0-9]{3}[0-9]{4}[0-9]{3}"
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="********"
+                id="password"
+                name="password"
+              />
+              <label htmlFor="BD">Birthday</label>
+              <input
+                value={BD}
+                onChange={(e) => setBD(e.target.value)}
+                type="date"
+                placeholder="BD"
+                id="BD"
+                name="BD"
+              />
+              <button type="submit">Sign In</button>
+              <button className="link-btn" onClick={navigateLogin}>
+                Already have an account? Login here.
+              </button>
+              <button className="button_SendHome" onClick={navigateSite}>
+                Back to Site
+              </button>
+            </div>
     </div>
+    </>
+        )}
+      </form>
   );
 };
+
