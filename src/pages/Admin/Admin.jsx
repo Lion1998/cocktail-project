d40fd088727930c02d3f999ade198360a3580968
לאון זולוטריוב <kinglion1964@gmail.com>
2024-01-08 06:15:32 +0200

    update

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,28 +1,39 @@
-import { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./Admin.css";
 import "bootstrap";
 import { Context } from "../../Context";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Admin() {
   const { state } = useContext(Context);
-  const [orders, setOrders] = useState([]);
+  const [allOrders, setAllOrders] = useState([]);
+  const [displayedOrders, setDisplayedOrders] = useState([]);
   const [times, setTimes] = useState([]);
   const [user, setUser] = useState([]);
+  const [searchTermDate, setSearchTermDate] = useState("");
+  const [searchTermUser, setSearchTermUser] = useState("");
+  const [searchTermTime, setSearchTermTime] = useState("");
+  const [searchTermTable, setSearchTermTable] = useState("");
+  const navigate = useNavigate();
 
-  const navigate = useNavigate(); 
   const navigateSite = () => {
     navigate("/");
   };
- 
+
   const fetchData = async () => {
     try {
-      const times = await fetch("https://localhost:7213/Order/AriivalTime").then((res) => res.json());
-      const orders = await fetch("https://localhost:7213/Order/Orders").then((res) => res.json());
-      const user = await fetch("https://localhost:7213/Register").then((res) => res.json());
-      setTimes(times);
-      setOrders(orders);
-      setUser(user);
+      const timesData = await fetch(
+        "https://localhost:7213/Order/AriivalTime"
+      ).then((res) => res.json());
+      const ordersData = await fetch(
+        "https://localhost:7213/Order/Orders"
+      ).then((res) => res.json());
+      const userData = await fetch("https://localhost:7213/Register").then(
+        (res) => res.json()
+      );
+      setTimes(timesData);
+      setAllOrders(ordersData);
+      setUser(userData);
     } catch (error) {
       console.error("Failed to fetch data from the API:", error);
     }
@@ -32,21 +43,54 @@ export default function Admin() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    filterOrders();
+  }, [
+    searchTermDate,
+    searchTermUser,
+    searchTermTime,
+    searchTermTable,
+    allOrders,
+  ]);
+
+  const filterOrders = () => {
+    const filteredOrders = allOrders.filter(
+      (order) =>
+        order.date.toLowerCase().includes(searchTermDate.toLowerCase()) &&
+        user
+          .find((u) => u.id === order.userID)
+          .firstName.toLowerCase()
+          .includes(searchTermUser.toLowerCase()) &&
+        times
+          .find((t) => t.id === order.expectedArrivalID)
+          .time.toLowerCase()
+          .includes(searchTermTime.toLowerCase()) &&
+        order.tablesID
+          .toString()
+          .toLowerCase()
+          .includes(searchTermTable.toLowerCase())
+    );
+    setDisplayedOrders(filteredOrders);
+  };
+
   if (state.user.type !== "Admin") {
-    return navigateSite
+    return navigateSite;
   }
 
   const handleDeleteRow = async (id) => {
     try {
-      const response = await fetch(`https://localhost:7213/Delete/DeleteOrder/${id}`, {
-        method: "DELETE",
-      });
+      const response = await fetch(
+        `https://localhost:7213/Delete/DeleteOrder/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      setOrders((prevData) => prevData.filter((order) => order.id !== id));
+      allOrders((prevData) => prevData.filter((order) => order.id !== id));
     } catch (error) {
       console.error("שגיאה בבקשה למחיקת שורה מה-API:", error);
     }
@@ -55,9 +99,42 @@ export default function Admin() {
   return (
     <div className="admin_page">
       <div>
-        <h2>טבלת הזמנות</h2>
-        <table className="table ">
+        <h1>טבלת הזמנות</h1>
+        <table className="table">
           <thead>
+            <tr>
+              <th />
+              <th>
+                <input
+                  type="text"
+                  placeholder="חיפוש לפי משתמש..."
+                  onChange={(e) => setSearchTermUser(e.target.value)}
+                />
+              </th>
+              <th>
+                <input
+                  type="text"
+                  placeholder="חיפוש לפי זמן..."
+                  onChange={(e) => setSearchTermTime(e.target.value)}
+                />
+              </th>
+              <th>
+                <input
+                  type="text"
+                  placeholder="חיפוש לפי שולחן..."
+                  onChange={(e) => setSearchTermTable(e.target.value)}
+                />
+              </th>
+              <th>
+                <input
+                  type="text"
+                  placeholder="חיפוש לפי תאריך..."
+                  onChange={(e) => setSearchTermDate(e.target.value)}
+                />
+              </th>
+              <th></th>
+              <th></th>
+            </tr>
             <tr>
               <th>ID</th>
               <th>User</th>
@@ -68,15 +145,20 @@ export default function Admin() {
             </tr>
           </thead>
           <tbody>
-            {orders.map((order) => (
+            {displayedOrders.map((order) => (
               <tr key={order.id}>
                 <td>{order.id}</td>
                 <td>{user.find((u) => u.id === order.userID).firstName}</td>
-                <td>{times.find((u) => u.id === order.expectedArrivalID).time}</td>
+                <td>
+                  {times.find((t) => t.id === order.expectedArrivalID).time}
+                </td>
                 <td>{order.tablesID}</td>
                 <td>{order.date.split("T")[0]}</td>
                 <td>
-                  <button className="btn btn-danger" onClick={() => handleDeleteRow(order.id)}>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDeleteRow(order.id)}
+                  >
                     Delete
                   </button>
                 </td>
